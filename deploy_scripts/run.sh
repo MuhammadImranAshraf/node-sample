c00619bd61a4e97510c60f89f3d5f4e89d9f2a21
MuhammadImranAshraf <imranashraf@gordiansol.com>
2023-10-15 19:57:08 +0200

    cht script

--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+# Navigate to the desired directory
 cd /home/ec2-user
-mkdir node
+
+# Ensure the directory is available or create it
+mkdir -p node
 cd node/
+
+# Ensure NVM is loaded into the shell session, adjust the path as per your setup
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install the dependencies and start the application using PM2 to daemonize it
 npm install
+
+# Start the application using PM2 to keep it running in the background
 npm start
