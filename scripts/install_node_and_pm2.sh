6b94a4c774f4f1237e5f0cb58480ac662563e408
MuhammadImranAshraf <imranashraf@gordiansol.com>
2023-10-15 14:11:06 +0200

    pm2 stop

--- scripts/install_node_and_pm2.sh
+++ scripts/install_node_and_pm2.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 # Update package lists
-apt-get update
-
-# Install Node.js 18.x
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt-get install -y nodejs
-
-# Install PM2
-npm install -g pm2
+pm2 stop all
