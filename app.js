e9787d8abc6fc574b6fd8e34ad3a74d93f3358ad
MuhammadImranAshraf <imranashraf@gordiansol.com>
2023-10-15 18:37:55 +0200

    codebuild change

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const os = require("os");
+const hostname = os.hostname();
+
+console.log(hostname);
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("pages/index", { hostname: hostname, title: "home" });
+});
+
+app.get("/about", function (req, res) {
+  res.render("pages/about", { title: "about" });
+});
+
+app.listen(8080, function () {
+  console.log("server is up on port 8080");
+});
